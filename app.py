cc53c8c351df2c621084434b187726eb7bf3d1c4
adithi14 <adsa20cs@cmrit.ac.in>
2022-12-20 22:40:31 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask,request,render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+
+def index():
+    return "index page"
+
+@app.route("/user/<Adithi>")
+def profile(Adithi):
+    return f"{Adithi}\'s profile"
+
+'''@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method=="POST":
+        return do_the_login()
+    else:
+        return show_login_form()'''
+
+@app.route("/hello")
+def hello_world():
+    return render_template("index.html")
